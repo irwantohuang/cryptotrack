836038a31c7e42f3d456ee11428b017a96428188
Irwanto Huang <irwanto.ng11@gmail.com>
2024-09-04 07:57:56 +0700

    [ADD] Overview Section - Home Page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,11 @@
 import Hero from "../views/home/Hero"
+import Overview from "../views/home/Overview"
 
 const HomePage = () => {
     return (
-        // <div className="flex w-full items-center justify-center h-screen bg-hero-bg-gradient bg-cover bg-no-repeat">
-        //     <h1 >Home Page</h1>
-        // </div>
-
         <div className="w-full h-auto overflow-x-hidden">
             <Hero />
+            <Overview />
         </div>
     )
 }
