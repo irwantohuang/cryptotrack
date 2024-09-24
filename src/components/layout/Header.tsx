4c2ef93cf200de06c4f1cdec80de799dbfc1a1c4
Irwanto Huang <irwanto.ng11@gmail.com>
2024-09-24 23:46:29 +0700

    changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -10,7 +10,7 @@ const Header = ({ children }: HeaderProps) => {
         <header className="header p-4 fixed bottom-0 md:top-0 md:bottom-auto left-0 w-full z-[999]">
             <div className="container mx-auto px-4">
                 <div className="flex items-center justify-center md:justify-between bg-gradient-to-r from-primary-black/50 via-primary-black-300/50 to-primary-black/50 px-8 py-3 rounded backdrop-filter backdrop-blur-lg bg-opacity-50 shadow">
-                    <Link to="/" className="text-primary-white hover:text-primary-white-200 hidden md:inline-block font-bold text-2xl lowercase transition-all duration-200">cryptotrack.</Link>
+                    <Link to="/" className="text-primary-white hover:text-accent hidden md:inline-block font-bold text-2xl lowercase transition-all duration-200">cryptotrack.</Link>
 
                     {/* Navbar */}
                     {children}
